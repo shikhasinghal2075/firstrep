bd6361f36c63fbdb6d9d0ab18e7ce0ea3630d8cd
Shikha Singhal <60216456+shikhasinghal2075@users.noreply.github.com>
2021-10-05 15:05:35 +0530

    Update ternary.c

--- ternary.c
+++ ternary.c
@@ -2,13 +2,10 @@
 
 int main()
 {
-    int a;
-    int b;
-    int c;
+    int a,b,c;
     printf("input three numbers:");
     scanf("%d%d%d",&a,&b,&c);
-//  working
+    //  working
     a>b ? b>c ? printf("%d<%d<%d",c,b,a) : printf("%d<%d<%d",b,c,a) : b<c ? printf("%d<%d<%d",a,b,c) : a<c ? printf("%d<%d<%d",a,c,b) : printf("%d<%d<%d",c,a,b);
-
     return 0;
 }
